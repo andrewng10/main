f3c3e38e4b047e64fbf6b3f094f3a06f5e20f1d5
Andrew Ng <andrewng7731@gmail.com>
2023-03-07 23:15:40 -0800

    Commit

---
+++ #24 Walrus Operator & Functions to Variables .py
@@ -0,0 +1,39 @@
+# Walrus operator   ":="
+# #assignment expression aka walrus operator and assigns values to variables as part of a larger expression
+
+#Example 1
+
+#foods = list()
+#while True:
+       #food = input("What food do you like?: ")
+       #if food == "quit":
+            #break
+       #foods.append(food)
+
+#foods = list()
+#while food := input("What food do you like?: ") !="quit":
+    #foods.append(food)
+
+
+
+#Example 2
+
+#happy = True       #combining both lines of code
+#print(happy)
+
+#print(happy := True)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+#def hello():
+    #print("Hello")
+
+#hi = hello
+#hello()
+#hi()
+
+say = print
+say("Whoa! I can't believe this works :O")
+
+
+
